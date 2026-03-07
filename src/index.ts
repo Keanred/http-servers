21800203e2150e4ad87af4b08ffb573ba3ded23d
Anssi Keinänen <135608429+anssi-voyantic@users.noreply.github.com>
2026-03-07 12:04:06 +0200

    Add log responses middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import { PORT } from './config.js';
 import express from 'express';
+import { readinessHandler } from './routes/readinessHandler.js';
+import { middlewareLogResponses } from './middlewares.js';
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
+app.use("/app", express.static("./src/app"));
+
+app.get('/healthz', readinessHandler);
+
+app.use(middlewareLogResponses);
+app.listen(PORT, () => {
+  console.log('Server is running on port http://localhost:8080');
 });
---
+++ src/middlewares.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express';
+
+type Middleware = (req: Request, res: Response, next: NextFunction) => void;
+
+export const middlewareLogResponses: Middleware = (req: Request, res: Response, next: NextFunction): void => {
+  res.on('finish', () => {
+    if (res.statusCode !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.path} - Status: ${res.statusCode}`);
+    }
+  });
+  next();
+}
